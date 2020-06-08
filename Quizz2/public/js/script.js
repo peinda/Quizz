4cd76a6a1cd9f629c2aad9db6f572bd509363bfa
peinda <62188447+peinda@users.noreply.github.com>
2020-06-08 05:53:01 +0000

    Add files via upload

---
+++ Quizz2/public/js/script.js
@@ -0,0 +1,30 @@
+$(document).ready(function(){
+    var error = false;
+    $('#form-connect').submit(function(){
+        if ($("#username").val().length===0) {
+            $("#username").after("<span>Merci de remplir ce champ</span>"); 
+            error = true;
+            event.preventDefault();   
+        }
+         if ($("#password").val().length===0) {
+            $("#password").after("<span>Merci de remplir ce champ</span>");  
+            error = true;
+            event.preventDefault();  
+        }
+        if(error == false){//si y'a pas d'erreur
+            let form_data=$(this).serializeArray();
+            $.post("./data/message.php",form_data,
+                function(result){
+                    if (result == '  admin') {
+                        window.location=`index.php?user=admin`;
+                    }
+                    else if (result == '  jeux') {
+                        window.location=`index.php?user=joueur`
+                    }
+                    else{
+                        alert('login ou mot de passe incorrect');
+                    }
+            })
+        }
+    })
+})
